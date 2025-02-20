dc52573499275c6271e040d2bff6df69b75d0b29
tranqui268 <trannhatqui712@gmail.com>
2025-02-20 21:00:07 +0700

    first commit

---
+++ src/auth/domain/IAuthRepository.ts
@@ -0,0 +1,6 @@
+import User from "./User";
+
+export default interface IAuthRepository{
+    find(email : string): Promise<User>
+    add(name : string, email : string, password : string, type : string) : Promise<string> 
+}
